38f9816220d52fd16dbaed19fd49d359f5af106a
MaPirlet <matthiaspirlet@gmail.com>
2024-03-08 16:31:20 +0100

    Add lab 5 on Flask

---
+++ labs/05_flask/src/jinja_app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
