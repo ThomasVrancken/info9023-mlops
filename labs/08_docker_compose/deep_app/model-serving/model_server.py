e0f42106ed8f682a64e1684b7c0b1c79960a20ee
MaPirlet <matthiaspirlet@gmail.com>
2024-04-02 10:09:00 +0200

    Add better port management

--- labs/08_docker_compose/deep_app/model-serving/model_server.py
+++ labs/08_docker_compose/deep_app/model-serving/model_server.py
@@ -23,4 +23,4 @@ def predict():
     return jsonify({'prediction': output.item()})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(debug=True)
--- labs/08_docker_compose/deep_app/web-application/app.py
+++ labs/08_docker_compose/deep_app/web-application/app.py
@@ -46,4 +46,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True)
